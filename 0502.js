191a88feb6f79ec1a48013115971cc14735aa24c
Seka Shchek <DoomkaSU@gmail.com>
2015-02-25 15:33:52 +0200

    changes_25.02

--- 0502.js
+++ 0502.js
@@ -5,6 +5,8 @@
 bufferForInformation = [];
 number = 0;
 number1 = 0;
+keyForSorting = false;
+keyForSortDirections = false;
 
 
 radyFunction = function() {
@@ -16,93 +18,82 @@ radyFunction = function() {
 $(radyFunction);
 
 
-renderOneMessage = function (information) {
-    number++;
-    $('tbody').append(
-        "<tr>" +
-            "<td>" + number +                   "</td>" +
-            "<td>" + information.street +       "</td>" +
-            "<td>" + information.city +         "</td>" +
-            "<td>" + information.zip +          "</td>" +
-            "<td>" + information.state +        "</td>" +
-            "<td>" + information.beds +         "</td>" +
-            "<td>" + information.baths +        "</td>" +
-            "<td>" + information.sq__ft +       "</td>" +
-            "<td>" + information.type +         "</td>" +
-            "<td>" + information.sale_date +    "</td>" +
-            "<td>" + information.price +        "</td>" +
-            "<td>" + information.latitude +     "</td>" +
-            "<td>" + information.longitude +    "</td>" +
-        "</<tr>>");
-};
-
-
 addInformInBufForForm = function (answerFromServer) {
     bufferForInformation = answerFromServer;
     //console.log(bufferForInformation.length);
-    takeMasseges();
+    renderInformation();
 };
 
 
 attachListeners = function () {
     $('.forSymbols').on ("click", function clickForSort (event)   {
         var elem = $(event.target),
-            isUp = elem.hasClass("symbolDown");
+            isDown = elem.hasClass("symbolDown");
+
+        keyForSorting = $(elem).parent().attr("sortkey");
+
         console.log(elem);
+        console.log($(elem).parent().attr("sortkey"));
         $('.forSymbols').removeClass("symbolUp")
             .removeClass("symbolDown")
             .addClass("symbolNotSort");
         elem.removeClass("symbolNotSort");
-        if (isUp) {
+        if (isDown) {
             elem.addClass("symbolUp");
+            keyForSortDirections = false;
         }
         else{
             elem.addClass("symbolDown");
+            keyForSortDirections = true;
         }
+        //attachListeners();
+        sort();
     });
 };
 
 
-takeMasseges = function () {
-    takeTheKeysInTheObject();
-    sort();
-    attachListeners();
-    var l = bufferForInformation.length,
-        i;
-    for (i=0;i < l; i++) { // проходит по индексам массива(у нас там массив с объектами)
-    renderOneMessage(bufferForInformation[i]);
-    }
-    // и то, и то работает одинаково!!!!!!
-    /*for (i in bufferForInformation) { // работает как по массиву, только проходит по ключам объектов
-    //(каждый наш объект получает ключ(индекс) от 0 и ..., но названия настоящих ключей не меняются)
-    // зачем это нужно?????????????
-        renderOneMessage(bufferForInformation[i]);
-    }
-    */
-};
-
-information
-
-
-
-displayOneColumns = function (key) {
-        number1++;
-    $('.trColomnNumbers').append("<td>" + number1 +"</td>");
-    $('.trColomnTitles').append("<td>" + key + "<span class='forSymbols symbolNotSort'></span></td>");
-};
-
-
-takeTheKeysInTheObject = function () {
-    var key;
+renderInformation = function () {
+    var key, l = bufferForInformation.length, i;
     for (key in bufferForInformation[0]) {
-        displayOneColumns(key);
+        //відмальовує дві головні(основні) строки
+        number1++;
+        $('tbody').append("<td>" + number1 +"</td>");//номери
+        //назви колонок
+        $('.trColomnTitles').append("<td sortkey = '" + key + "'>" + key + "<span class='forSymbols symbolNotSort'></span></td>");
     }
+    for (i=0;i < l; i++) { // проходит по индексам массива(у нас там массив с объектами)
+        // и то, и то работает одинаково!!!!!!
+        /*for (i in bufferForInformation) { // работает как по массиву, только проходит по ключам объектов
+         //(каждый наш объект получает ключ(индекс) от 0 и ..., но названия настоящих ключей не меняются)
+         // зачем это нужно?????????????
+         renderOneMessage(bufferForInformation[i]);
+         }
+         */
+        number++;
+        $('tbody').append(
+            "<tr>" +
+            "<td>" + number +                               "</td>" +
+            "<td>" + bufferForInformation[i].street +       "</td>" +
+            "<td>" + bufferForInformation[i].city +         "</td>" +
+            "<td>" + bufferForInformation[i].zip +          "</td>" +
+            "<td>" + bufferForInformation[i].state +        "</td>" +
+            "<td>" + bufferForInformation[i].beds +         "</td>" +
+            "<td>" + bufferForInformation[i].baths +        "</td>" +
+            "<td>" + bufferForInformation[i].sq__ft +       "</td>" +
+            "<td>" + bufferForInformation[i].type +         "</td>" +
+            "<td>" + bufferForInformation[i].sale_date +    "</td>" +
+            "<td>" + bufferForInformation[i].price +        "</td>" +
+            "<td>" + bufferForInformation[i].latitude +     "</td>" +
+            "<td>" + bufferForInformation[i].longitude +    "</td>" +
+            "</<tr>>");
+    }
+    attachListeners();
 };
 
 requestMasseges = function() {
     $.ajax({url:"convertcsv.json"
 })
-.done(takeTheKeysInTheObject, addInformInBufForForm);
+.done(addInformInBufForForm);
 };
 
 
@@ -115,14 +106,26 @@ sort = function () {
     console.log(l);
     for (c = 0; c < l; c++) {
         for (i = 0; i < l - 1; i++) {
-            if (bFI[i].key > bFI[i+1].key) {
-                buf = bFI[i];
-                bFI[i] = bFI[i+1];
-                bFI[i+1] = buf;
+            if (keyForSortDirections === false) {
+                if (bFI[i][keyForSorting] > bFI[i + 1][keyForSorting]) {
+                    buf = bFI[i];
+                    bFI[i] = bFI[i + 1];
+                    bFI[i + 1] = buf;
+                    //console.log(bFI[0].street, "1");
+                }
+            } else {
+                if (bFI[i][keyForSorting] < bFI[i + 1][keyForSorting]) {
+                    buf = bFI[i];
+                    bFI[i] = bFI[i + 1];
+                    bFI[i + 1] = buf;
+                    //console.log(bFI[0].street, "2");
+                }
             }
         }
     }
-    console.log(bFI[0].street);
+    $('tbody').empty();
+    renderInformation();
+    console.log(bFI[0].street, "3");
 };
 
 
